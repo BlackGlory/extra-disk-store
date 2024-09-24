6b805ae4ab5ad30e619f32feafb3c68d538c6a5b
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2024-09-24 17:08:43 +0000

    perf: fix benchmarks

--- benches/index.ts
+++ benches/index.ts
@@ -8,141 +8,41 @@ import {
 , JSONValueConverter
 } from '../lib/index.js'
 import { createTempName, remove } from 'extra-filesystem'
-import fs from 'fs/promises'
-import prettyBytes from 'pretty-bytes'
-import * as LMDB from 'lmdb'
 import { LRUMap } from '@blackglory/structures'
 
 const benchmark = new Benchmark('I/O performance')
 
 go(async () => {
-  benchmark.addCase('Map (write)', async () => {
-    const map = new Map<string, string>()
+  benchmark.addCase('DiskStore (write)', async () => {
+    const filename = await createTempName()
+    const store = await DiskStore.create(filename)
+    const view = new DiskStoreView(
+      store
+    , new IndexKeyConverter()
+    , new JSONValueConverter()
+    )
 
     return {
       beforeEach() {
-        map.clear()
+        store.clear()
       }
     , iterate() {
-        for (let i = 1000; i--;) {
-          map.set(`${i}`, JSON.stringify(i))
-        }
-      }
-    }
-  })
-
-  benchmark.addCase('LMDB (write, non-concurrent)', async () => {
-    const dirname = await createTempName()
-    const db = LMDB.open(dirname, {
-      compression: false
-    , encoding: 'binary'
-    })
-
-    return {
-      async beforeEach() {
-        await db.clearAsync()
-      }
-    , async iterate() {
-        for (let i = 1000; i--;) {
-          await db.put(`${i}`, Buffer.from(JSON.stringify(i)))
+        for (let i = 100; i--;) {
+          view.set(i, i)
         }
       }
     , async afterAll() {
-        await db.close()
-
-        const { size } = await fs.stat(dirname)
-        console.log(`LMDB (write) size: ${prettyBytes(size)}`)
-
-        await remove(dirname)
-      }
-    }
-  })
-
-  benchmark.addCase('LMDB (write, concurrent)', async () => {
-    const dirname = await createTempName()
-    const db = LMDB.open(dirname, {
-      compression: false
-    , encoding: 'binary'
-    })
-
-    return {
-      async beforeEach() {
-        await db.clearAsync()
-      }
-    , async iterate() {
-        const promises: Array<Promise<unknown>> = []
-        for (let i = 1000; i--;) {
-          promises.push(db.put(`${i}`, Buffer.from(JSON.stringify(i))))
-        }
-        await Promise.all(promises)
-      }
-    , async afterAll() {
-        await db.close()
-
-        const { size } = await fs.stat(dirname)
-        console.log(`LMDB (write) size: ${prettyBytes(size)}`)
-
-        await remove(dirname)
-      }
-    }
-  })
-
-  benchmark.addCase('DiskStore (write, non-concurrent)', async () => {
-    const store = new DiskStore()
-    const view = new DiskStoreView(
-      store
-    , new IndexKeyConverter()
-    , new JSONValueConverter()
-    )
-
-    return {
-      async beforeEach() {
-        await store.clear()
-      }
-    , async iterate() {
-        for (let i = 1000; i--;) {
-          await view.set(i, i)
-        }
-      }
-    , async afterAll() {
-        const { size } = await fs.stat(store._dirname)
-        console.log(`DiskStore (write) size: ${prettyBytes(size)}`)
-
-        await store.close()
-      }
-    }
-  })
-
-  benchmark.addCase('DiskStore (write, concurrent)', async () => {
-    const store = new DiskStore()
-    const view = new DiskStoreView(
-      store
-    , new IndexKeyConverter()
-    , new JSONValueConverter()
-    )
-
-    return {
-      async beforeEach() {
-        await store.clear()
-      }
-    , async iterate() {
-        const promises: Array<Promise<unknown>> = []
-        for (let i = 1000; i--;) {
-          promises.push(view.set(i, i))
-        }
-        await Promise.all(promises)
-      }
-    , async afterAll() {
-        const { size } = await fs.stat(store._dirname)
-        console.log(`DiskStore (write) size: ${prettyBytes(size)}`)
-
-        await store.close()
+        store.close()
+        await remove(filename)
       }
     }
   })
 
   benchmark.addCase('DiskStoreWithCache (write)', async () => {
-    const store = new DiskStoreWithCache(new DiskStore(), new LRUMap(1000))
+    const filename = await createTempName()
+    const diskStore = await DiskStore.create(filename)
+    const memoryCache = new LRUMap<string, any>(100)
+    const store = new DiskStoreWithCache(diskStore, memoryCache)
     const view = new DiskStoreView(
       store
     , new IndexKeyConverter()
@@ -150,194 +50,121 @@ go(async () => {
     )
 
     return {
-      async beforeEach() {
-        await store.clear()
+      beforeEach() {
+        store.clear()
       }
-    , async iterate() {
-        const promises: Array<Promise<unknown>> = []
-        for (let i = 1000; i--;) {
-          promises.push(view.set(i, i))
+    , iterate() {
+        for (let i = 100; i--;) {
+          view.set(i, i)
         }
-        await Promise.all(promises)
       }
     , async afterAll() {
-        await store.close()
-      }
-    }
-  })
-
-  benchmark.addCase('Map (overwrite)', async () => {
-    const map = new Map<string, string>()
-    for (let i = 1000; i--;) {
-      map.set(`${i}`, JSON.stringify(i))
-    }
-
-    return () => {
-      for (let i = 1000; i--;) {
-        map.set(`${i}`, JSON.stringify(i))
-      }
-    }
-  })
-
-  benchmark.addCase('LMDB (overwrite)', async () => {
-    const dirname = await createTempName()
-    const db = LMDB.open(dirname, {
-      compression: false
-    , encoding: 'binary'
-    })
-    for (let i = 1000; i--;) {
-      await db.put(`${i}`, Buffer.from(JSON.stringify(i)))
-    }
-
-    return {
-      async iterate() {
-        const promises: Array<Promise<unknown>> = []
-        for (let i = 1000; i--;) {
-          promises.push(db.put(`${i}`, Buffer.from(JSON.stringify(i))))
-        }
-        await Promise.all(promises)
-      }
-    , async afterAll() {
-        await db.close()
-        await remove(dirname)
+        store.close()
+        await remove(filename)
       }
     }
   })
 
   benchmark.addCase('DiskStore (overwrite)', async () => {
-    const store = new DiskStore()
+    const filename = await createTempName()
+    const store = await DiskStore.create(filename)
     const view = new DiskStoreView(
       store
     , new IndexKeyConverter()
     , new JSONValueConverter()
     )
-    for (let i = 1000; i--;) {
-      await view.set(i, i)
+    for (let i = 100; i--;) {
+      view.set(i, i)
     }
 
     return {
-      async iterate() {
-        const promises: Array<Promise<unknown>> = []
-        for (let i = 1000; i--;) {
-          promises.push(view.set(i, i))
+      iterate() {
+        for (let i = 100; i--;) {
+          view.set(i, i)
         }
-        await Promise.all(promises)
       }
     , async afterAll() {
-        await store.close()
+        store.close()
+        await remove(filename)
       }
     }
   })
 
   benchmark.addCase('DiskStoreWithCache (overwrite)', async () => {
-    const store = new DiskStoreWithCache(new DiskStore(), new LRUMap(1000))
+    const filename = await createTempName()
+    const diskStore = await DiskStore.create(filename)
+    const memoryCache = new LRUMap<string, any>(100)
+    const store = new DiskStoreWithCache(diskStore, memoryCache)
     const view = new DiskStoreView(
       store
     , new IndexKeyConverter()
     , new JSONValueConverter()
     )
-    for (let i = 1000; i--;) {
-      await view.set(i, i)
+    for (let i = 100; i--;) {
+      view.set(i, i)
     }
 
     return {
-      async iterate() {
-        const promises: Array<Promise<unknown>> = []
-        for (let i = 1000; i--;) {
-          promises.push(view.set(i, i))
+      iterate() {
+        for (let i = 100; i--;) {
+          view.set(i, i)
         }
-        await Promise.all(promises)
       }
     , async afterAll() {
-        await store.close()
-      }
-    }
-  })
-
-  benchmark.addCase('Map (read)', async () => {
-    const map = new Map<string, string>()
-    for (let i = 1000; i--;) {
-      map.set(`${i}`, JSON.stringify(i))
-    }
-
-    return () => {
-      for (let i = 1000; i--;) {
-        map.set(`${i}`, JSON.stringify(i))
-      }
-    }
-  })
-
-  benchmark.addCase('LMDB (read)', async () => {
-    const dirname = await createTempName()
-    const db = LMDB.open(dirname, {
-      compression: false
-    , encoding: 'binary'
-    })
-    for (let i = 1000; i--;) {
-      await db.put(`${i}`, Buffer.from(JSON.stringify(i)))
-    }
-
-    return {
-      async iterate() {
-        const promises: Array<Promise<unknown>> = []
-        for (let i = 1000; i--;) {
-          promises.push(db.get(`${i}`))
-        }
-        await Promise.all(promises)
-      }
-    , async afterAll() {
-        await db.close()
-        await remove(dirname)
+        store.close()
+        await remove(filename)
       }
     }
   })
 
   benchmark.addCase('DiskStore (read)', async () => {
-    const store = new DiskStore()
+    const filename = await createTempName()
+    const store = await DiskStore.create(filename)
     const view = new DiskStoreView(
       store
     , new IndexKeyConverter()
     , new JSONValueConverter()
     )
-    for (let i = 1000; i--;) {
-      await view.set(i, i)
+    for (let i = 100; i--;) {
+      view.set(i, i)
     }
 
     return {
-      async iterate() {
-        const promises: Array<Promise<unknown>> = []
-        for (let i = 1000; i--;) {
-          promises.push(view.get(i))
+      iterate() {
+        for (let i = 100; i--;) {
+          view.get(i)
         }
-        await Promise.all(promises)
       }
     , async afterAll() {
-        await store.close()
+        store.close()
+        await remove(filename)
       }
     }
   })
 
   benchmark.addCase('DiskStoreWithCache (read)', async () => {
-    const store = new DiskStoreWithCache(new DiskStore(), new LRUMap(1000))
+    const filename = await createTempName()
+    const diskStore = await DiskStore.create(filename)
+    const memoryCache = new LRUMap<string, any>(100)
+    const store = new DiskStoreWithCache(diskStore, memoryCache)
     const view = new DiskStoreView(
       store
     , new IndexKeyConverter()
     , new JSONValueConverter()
     )
-    for (let i = 1000; i--;) {
+    for (let i = 100; i--;) {
       await view.set(i, i)
     }
 
     return {
       async iterate() {
-        const promises: Array<Promise<unknown>> = []
-        for (let i = 1000; i--;) {
-          promises.push(view.get(i))
+        for (let i = 100; i--;) {
+          view.get(i)
         }
-        await Promise.all(promises)
       }
     , async afterAll() {
-        await store.close()
+        store.close()
+        await remove(filename)
       }
     }
   })
